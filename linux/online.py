9b1ad31824b2f3e16b4d2e6cb5d9eda67e203c90
DenisKoleda <89728650+DenisKoleda@users.noreply.github.com>
2022-02-07 15:59:37 +0400

    Create online.py
    Команды на различный вывод информации о количестве игроков по RCON

---
+++ linux/online.py
@@ -0,0 +1,26 @@
+from rcon import Client
+
+
+# Отображение списка игроков в онлайне
+with Client('109.195.19.162', 27015, passwd='8TNvxsxJDp') as client:
+    online = client.run('players')
+
+online = "\n".join(online.split('\n')[1:-1])
+
+print(online)
+
+# Отображение игроков в Array
+with Client('109.195.19.162', 27015, passwd='8TNvxsxJDp') as client:
+    online = client.run('players')
+
+online = online.split('\n')[1:-1]
+
+print(online)
+
+# Отображение колличества игроков
+with Client('109.195.19.162', 27015, passwd='8TNvxsxJDp') as client:
+    online = client.run('players')
+
+online = online.split('\n')[0]
+
+print(online)
